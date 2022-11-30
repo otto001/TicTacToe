07cca8044c8f5b607f9f1e02a15f614e59011e3e
Matteo Ludwig <ludwig@silpion.de>
2022-11-30 13:49:29 +0100

    Initial Commit

---
+++ Tic Tac Toe Watch App/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic Tac Toe Watch App
+//
+//  Created by Matteo Ludwig on 30.11.22.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_Toe_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tic Tac Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic Tac Toe
+//
+//  Created by Matteo Ludwig on 30.11.22.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
